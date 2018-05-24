1caba3e77dec3398211911e3f7a506db8f37034e
Josiah McPeak <mcpeakjosiah@gmail.com>
2018-05-24 14:11:52 -0700

    changes to my fork

--- PathWaze/dist/PathWaze/main.js
+++ PathWaze/dist/PathWaze/main.js
@@ -412,7 +412,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Geran Pauley\Desktop\Dojo\MEAN\Project\PathWaze\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! /Users/josiahmcpeak/Desktop/coding_dojo/PathWaze/PathWaze/src/main.ts */"./src/main.ts");
 
 
 /***/ })

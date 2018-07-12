79ea6ed9ff6cbdac95a635a3c07bc09c74ba22ef
Josiah McPeak <mcpeakjosiah@gmail.com>
2018-07-12 13:19:58 -0700

    error that chad is having too

--- PathWaze/dist/PathWaze/polyfills.js
+++ PathWaze/dist/PathWaze/polyfills.js
@@ -5472,7 +5472,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! /Users/josiahmcpeak/Desktop/coding_dojo/PathWaze/PathWaze/src/polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! /Users/josiahmcpeak/Desktop/PathWaze/PathWaze/src/polyfills.ts */"./src/polyfills.ts");
 
 
 /***/ })
